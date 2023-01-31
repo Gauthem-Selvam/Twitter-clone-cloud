01a1588caca5c4e95731ed517d5ba6628062af39
Gauthem Selvam <Gauthemselvam@techis.io>
2023-02-01 04:02:57 +0530

    host updated

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-1mi8xir429wn7epay3eizi%=-%ilufh6_*&c_&rb_1jj+2&k2r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
